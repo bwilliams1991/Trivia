e3573a0aa23ea2dfcddc0975e4350a802a871201
ben <bwilly91@msn.com>
2018-02-22 19:39:21 -0700

    buttons need rework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-
+$(document).ready(function () {
 
 // // Varible Declarations
 // --------------------------------------------------------------------------------------
@@ -9,153 +9,90 @@
 var questions = [
   {
     question: "Console release year?",
-    answers: {
-      a: "1990",
-      b: "1991",
-			x: "1992",
-			y: "1993"
-    },
-    correctAnswer: "b"
+    answers: ["1990", "1991", "1992", "1993"],
+    correctAnswer: "1991"
   },
   {
     question: "The Super Nintendo video/audio output was in how many bits?",
-    answers: {
-      a: "16 bit",
-      b: "8 bit",
-			x: "32 bit",
-			y: "9 bit"
-    },
-    correctAnswer: "a"
+    answers: [ "16 bit", "8 bit", "32 bit", "9 bit"],
+    correctAnswer: "16 bit"
   },
   {
     question: "Which game was not release with the console?",
-    answers: {
-      a: "Street Fighter II",
-      b: "Donkey Kong Country",
-			x: "Super Mario World",
-			y: "Super Mario Kart"
-    },
-    correctAnswer: "a"
+    answers: [ "Street Fighter II", "Donkey Kong Country", "Super Mario World", "Super Mario Kart"],
+    correctAnswer: "Street Fighter II"
 	},
 	{
     question: "What console was the predessor of the Super Nintendo?",
-    answers: {
-      a: "Nintendo 64",
-      b: "Sega Dreamcast",
-			x: "Atari",
-			y: "Nintendo"
-    },
-    correctAnswer: "y"
+    answers: [ "Nintendo 64", "Sega Dreamcast", "Atari", "Nintendo"],
+    correctAnswer: "Nintendo"
 	},
 	{
     question: "What console was the successor of the Super Nintendo?",
-    answers: {
-			a: "Nintendo 64",
-      b: "Sega Dreamcast",
-			x: "Atari",
-			y: "Nintendo"
-    },
-    correctAnswer: "a"
+    answers: ["Nintendo 64", "Sega Dreamcast", "Atari", "Nintendo"],
+    correctAnswer: "Nintendo 64"
 	},
 	{
-    question: "Media the game was on?",
-    answers: {
-      a: "Compact disc",
-      b: "Cassette",
-			x: "none of these...",
-			y: "Cartridge"
-    },
-    correctAnswer: "y"
+    question: "Media are the SNES game created on?",
+    answers: [ "Compact disc", "Cassette", "none of these...", "Cartridge"],
+    correctAnswer: "Cartridge"
 	},
 	{
     question: "Which game was remade on the Nintendo 64?",
-    answers: {
-      a: "Super Mario World",
-      b: "MegaMan X",
-			x: "Star Fox",
-			y: "Donkey Kong Country"
-    },
-    correctAnswer: "x"
+    answers: [ "Super Mario World", "MegaMan X", "Star Fox", "Donkey Kong Country"],
+    correctAnswer: "Star Fox"
 	},
 	{
     question: "AKA Rockman",
-    answers: {
-      a: "Mario",
-      b: "MegaMan",
-			x: "Luigi",
-			y: "Samus"
-    },
-    correctAnswer: "b"
+    answers: [ "Mario", "MegaMan", "Luigi", "Samus"],
+    correctAnswer: "MegaMan"
 	},
 	{
     question: "Which of these was not a SNES game?",
-    answers: {
-      a: "Final Fantasy VII",
-      b: "NBA Jam",
-			x: "Monopoly",
-			y: "Mortal Kombat II"
-    },
-    correctAnswer: "a"
+    answers: [ "Final Fantasy VII", "NBA Jam", "Monopoly", "Mortal Kombat II"],
+    correctAnswer: "Final Fantasy VII"
 	},
 	{
     question: "Super Mario World is what type of game?",
-    answers: {
-      a: "Adventure",
-      b: "RPG",
-			x: "Side-scrolling Platformer",
-			y: "Shooter"
-    },
-    correctAnswer: "x"
+    answers: ["Adventure", "RPG", "Side-scrolling Platformer", "Shooter"],
+    correctAnswer: "Side-scrolling Platformer"
 	},
 	{
-    question: "In the game Super Metroid the playable cahracter was...",
-    answers: {
-      a: "Metroid",
-      b: "Samus",
-			x: "Prime",
-			y: "Ridley"
-    },
-    correctAnswer: "b"
+    question: "In the game Super Metroid the playable character was...",
+    answers: ["Metroid", "Samus", "Prime", "Ridley"],
+    correctAnswer: "Samus"
 	},
 	{
 		question: "How many games for the SNES have the word 'Super' in them?",
-    answers: {
-      a: "241",
-      b: "200",
-			x: "211",
-			y: "248"
-    },
-    correctAnswer: "a"
+    answers: ["241", "200", "211", "248"],
+    correctAnswer: "241"
 	},
 	{
     question: "Who is the final boss in Super Mario World?",
-    answers: {
-      a: "Luigi",
-      b: "The Koopalings",
-			x: "Princess Toadstool",
-			y: "King Bowser"
-    },
-    correctAnswer: "y"
+    answers: ["Luigi", "The Koopalings", "Princess Toadstool", "King Bowser"],
+    correctAnswer: "King Bowser"
 	},
 	{
     question: "Number of playable characters in Super Mario Kart.",
-    answers: {
-      a: "8",
-      b: "6",
-			x: "7",
-			y: "10"
-    },
-    correctAnswer: "a"
+    answers: ["8","6","7","10"],
+    correctAnswer: "8"
 	}
 ];
+
+
+// Functions
+// --------------------------------------------------------------------------------------
+//
+
 
 // the object of the game... hahahahahaha
 var theWholeGame = {
 	questions: questions,
-	currentQ: 0,
-	timer: 30, //seconds
+	currentQA: 0,
+	timer: 5, //seconds
 	numCorrect: 0,
 	numInCorrect: 0,
+	unanswered: 0,
 
 	//function/methods
 	finalCountdown: function() {
@@ -172,49 +109,101 @@ var theWholeGame = {
 		}
 
 	},
-	addQ: function() {
+	addQA: function() {
 
+			// create timer
 		// adding timer var to an interval of 1 sec
 		timer = setInterval(theWholeGame.finalCountdown, 1000);
-		$("#").html("<h3>" + questions.question[theWholeGame.currentQ].question + "</h3>");
+		$("#timer").append(" " + timer);
 
-		for(var i = 0; i , questions; i++) {
 
+		$("#display").html("<h2>" + questions[theWholeGame.currentQA].question + "</h2>");
+
+		// adding buttons dynamically each time a new question is loaded
+		for(var i = 0; i < questions[theWholeGame.currentQA]; i++) {
+			$("#buttons").append
+
+			// $('#buttons').append('<button class="answer-button" id="button-' + i + '" data-name="' + questions[theWholeGame.currentQA].answers[i] + '">' + questions[theWholeGame.currentQA].answers[i] + '</button>');
 		}
 
 	},
-	nextQ: function() {
+	nextQA: function() {
+
+		// reset timer
+		theWholeGame.timer = 5;
+		$("#timer").html(theWholeGame.timer);
+
+		// 
+		$("#timer").html(theWholeGame.timer);
+		theWholeGame.currentQA++;
+		theWholeGame.addQA();
 
 	},
 	overNow: function() {
+		clearInterval(timer);
+		theWholeGame.unanswered++;
+		$("#display").html("<h2>You have run out of time...</h2>");
+		$("#display").append("<h3>The correct answer was..." + questions[theWholeGame.currentQA].correctAnswer + "</h3>");
 
+		if(theWholeGame.currentQA == questions.length - 1) {
+			setTimeout(theWholeGame.stats, 3000);
+		} else {
+			setTimeout(theWholeGame.nextQA, 3000);
+		};
 	},
 	stats: function() {
-
+		clearInterval(timer);
+		$("#display").html("<h2>Thats it.</h2>");
+		$("#stats").append("<h3>Correct: " + theWholeGame.numCorrect + "</h3>");
+		$("#stats").append("<h3>Incorrect: " + theWholeGame.numInCorrect + "</h3>");
+		$("#stats").append("<h3>Unanswered: " + theWholeGame.unanswered + "</h3>");
+		$("#stats").append("<button id='reset'>RESET</button>");
 	},
 	clickedOn: function() {
-
+		clearInterval(timer);
+		if ($(e.target).data("name") == questions[theWholeGame.currentQA].correctAnswer) {
+			theWholeGame.yourRight();
+		} else {
+			theWholeGame.yourWrong();
+		};
 	},
 	yourRight: function() {
+		// testing
+		console.log("yep");
 
+		clearInterval(timer);
+		theWholeGame.numCorrect++;
+		$("#display").html('<h2>Correct!</h2>');
+		if(theWholeGame.currentQA == questions.length - 1) {
+			setTimeout(theWholeGame.stats, 3000);
+		} else {
+			setTimeout(theWholeGame.nextQA, 3000);
+		};
 	},
 	yourWrong: function() {
+			// testing
+			console.log("nope");
+
+			clearInterval(timer);
+		theWholeGame.numInCorrect++;
+		$("#display").html('<h2>You are the weakest link, goodbye!</h2>');
+		$("#display").html("<h3>The correct answer was..." + questions[theWholeGame.currentQA].correctAnswer + "</h3>");
+		if(theWholeGame.currentQA == questions.length - 1) {
+			setTimeout(theWholeGame.stats, 3000);
+		} else {
+			setTimeout(theWholeGame.nextQA, 3000);
+		}
 
 	},
 	startOver: function() {
-
+		
 	}
 }
 
 
 
 
-// add references to HTML -ids
-// var questionHolder = $("#questions");
-// var statsHolder = $("#stats");
-// var buttonSelected = $("#buttons");
 
-// create timer
 	// switch questions at timer 0, set answer to wrong
 
 // display questions, based on timer
@@ -231,13 +220,29 @@ var theWholeGame = {
 
 
 
-alert("ready to start?");
+// alert("ready to start?");
 
-// Functions
-// --------------------------------------------------------------------------------------
-//
+
 
 // Process
 // --------------------------------------------------------------------------------------
 
+$("#start").on("click", function(){
+	$("#start").remove();
+	theWholeGame.addQA();
+});
 
+$("#reset").on("click", function(){
+	theWholeGame.startOver();
+});
+
+
+// on click of answer buttons links to clickedOn funct.
+$(document).on("click", '.answer-button', function(e){
+	theWholeGame.clickedOn(e);
+});
+
+// theWholeGame();
+
+
+});
